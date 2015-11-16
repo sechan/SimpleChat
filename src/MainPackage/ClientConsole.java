0d1e1093af22dd66accda9b73ef904ffa92211da
Andrew <ajrvester@gmail.com>
2015-11-15 19:56:41 -0500

    Phase two is complete. Could use some more feedback for events, less feedback for others.

--- src/MainPackage/ClientConsole.java
+++ src/MainPackage/ClientConsole.java
@@ -41,11 +41,11 @@ public class ClientConsole implements ChatIF
    * @param host The host to connect to.
    * @param port The port to connect on.
    */
-  public ClientConsole(String host, int port) 
+  public ClientConsole(String loginID, String host, int port) 
   {
     try 
     {
-      client= new ChatClient(host, port, this);
+      client= new ChatClient(loginID, host, port, this);
     } 
     catch(IOException exception) 
     {
@@ -111,10 +111,17 @@ public class ClientConsole implements ChatIF
   {
     String host = "";
     int port = 0;  //The port number
-
+    String loginID = null;
+    try{
+    	loginID = args[0];
+    }catch(ArrayIndexOutOfBoundsException e) {
+    	loginID = "testUser";
+//    	System.out.println("LoginID required to start client. Shutting down.");
+//    	System.exit(0);
+    }
     try
     {
-      host = args[0];
+      host = args[1];
     }
     catch(ArrayIndexOutOfBoundsException e)
     {
@@ -122,14 +129,14 @@ public class ClientConsole implements ChatIF
     }
     try
     {
-    	port = Integer.parseInt(args[1]);
+    	port = Integer.parseInt(args[2]);
     }
     catch(ArrayIndexOutOfBoundsException e)
     {
     	port=DEFAULT_PORT;
     }
     
-    ClientConsole chat= new ClientConsole(host, port);
+    ClientConsole chat= new ClientConsole(loginID, host, port);
     chat.accept();  //Wait for console data
   }
 }
--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -7,159 +7,161 @@ package client;
 import ocsf.client.*;
 import common.*;
 import java.io.*;
-import java.util.StringTokenizer; 
+import java.util.StringTokenizer;
 
 /**
- * This class overrides some of the methods defined in the abstract
- * superclass in order to give more functionality to the client.
+ * This class overrides some of the methods defined in the abstract superclass
+ * in order to give more functionality to the client.
  *
  * @author Dr Timothy C. Lethbridge
  * @author Dr Robert Lagani&egrave;
  * @author Fran&ccedil;ois B&eacute;langer
  * @version July 2000
  */
-public class ChatClient extends AbstractClient
-{
-  //Instance variables **********************************************
-  
-  /**
-   * The interface type variable.  It allows the implementation of 
-   * the display method in the client.
-   */
-  ChatIF clientUI; 
 
-  
-  //Constructors ****************************************************
-  
-  /**
-   * Constructs an instance of the chat client.
-   *
-   * @param host The server to connect to.
-   * @param port The port number to connect on.
-   * @param clientUI The interface type variable.
-   */
-  
-  public ChatClient(String host, int port, ChatIF clientUI) 
-    throws IOException 
-  {
-	
-    super(host, port); //Call the superclass constructor
-    this.clientUI = clientUI;
-    openConnection();
-  }
+public class ChatClient extends AbstractClient {
+	// Instance variables **********************************************
 
-  
-  //Instance methods ************************************************
-    
-  /**
-   * This method handles all data that comes in from the server.
-   *
-   * @param msg The message from the server.
-   */
-  public void handleMessageFromServer(Object msg) 
-  {
-    clientUI.display(msg.toString());
-  }
+	/**
+	 * The interface type variable. It allows the implementation of the display
+	 * method in the client.
+	 */
+	ChatIF clientUI;
+	private String loginID;
 
-  /**
-   * This method handles all data coming from the UI            
-   *
-   * @param message The message from the UI.    
-   */
-  public void handleMessageFromClientUI(String message)
-  {
-    try
-    {
-      sendToServer(message);
-    }
-    catch(IOException e)
-    {
-      clientUI.display
-        ("Could not send message to server.  Terminating client.");
-      quit();
-    }
-  }
-  
-  public void handleConsoleCommand(String message) {
-	  String arg = null;
-	  String command;
-	  StringTokenizer token = new StringTokenizer(message);
-	  command = token.nextToken();
-	  if (token.hasMoreTokens())
-		  arg = token.nextToken();
-	  switch(command) {
-	  case "#quit":
-		  quit();
-		  break;
-	  case "#logoff":
-		  if(isConnected()) {
-			try {
-				closeConnection();
-			} catch (IOException e) {
-				connectionException(e);
+	// Constructors ****************************************************
+
+	/**
+	 * Constructs an instance of the chat client.
+	 *
+	 * @param host
+	 *            The server to connect to.
+	 * @param port
+	 *            The port number to connect on.
+	 * @param clientUI
+	 *            The interface type variable.
+	 */
+
+	public ChatClient(String loginID, String host, int port, ChatIF clientUI) throws IOException {
+		super(host, port); // Call the superclass constructor
+		this.loginID = loginID;
+		this.clientUI = clientUI;
+		login();
+	}
+
+	// Instance methods ************************************************
+
+	/**
+	 * This method handles all data that comes in from the server.
+	 *
+	 * @param msg
+	 *            The message from the server.
+	 */
+	public void handleMessageFromServer(Object msg) {
+		clientUI.display(msg.toString());
+	}
+
+	/**
+	 * This method handles all data coming from the UI
+	 *
+	 * @param message
+	 *            The message from the UI.
+	 */
+	public void handleMessageFromClientUI(String message) {
+		try {
+			sendToServer(message);
+		} catch (IOException e) {
+			clientUI.display("Could not send message to server.  Terminating client.");
+			quit();
+		}
+	}
+
+	public void handleConsoleCommand(String message) {
+		String arg = null;
+		String command;
+		StringTokenizer token = new StringTokenizer(message);
+		command = token.nextToken();
+		if (token.hasMoreTokens())
+			arg = token.nextToken();
+		switch (command) {
+		case "#quit":
+			quit();
+			break;
+		case "#logoff":
+			if (isConnected()) {
+				try {
+					closeConnection();
+				} catch (IOException e) {
+					connectionException(e);
+				}
+			} else {
+				clientUI.display("Not currently logged in.");
 			}
-		  }else{
-			  clientUI.display("Not currently logged in.");
-		  }
-		  break;
-	  case "#setHost":
-		  if(!isConnected()) {
-			  setHost(arg);
-		  }
-		  else {
-			  clientUI.display("***ERROR*** Cannot change host while connected to server");
-		  }
-		  break;
-	  case "#setPort":
-		  if (!isConnected()) {
-			  setPort(Integer.parseInt(arg));
-		  }
-		  else {
-			  clientUI.display("***ERROR*** Cannot change port while connected to server");
-		  }
-		  break;
-	  case "#getHost" :
-		  clientUI.display("Host: " +getHost());
-		  break;
-	  case "#getPort" :
-		  clientUI.display("Port: " + getPort());
-		  break;
-	  case "#login" :
-		  if(!isConnected()) {
-			  try {
-				  clientUI.display("Logging on to " + getHost() + ":" +getPort());
-				  openConnection();
-			  } catch (IOException e) {
-				  connectionException(e);
-			  }
-		  }else{
-			  clientUI.display("Already logged in.");
-		  }
-		  break;
-	  }
-  }
-  
-  public void connectionException(Exception exception) {
-	  clientUI.display("Lost connection to server.\nQuitting...");
-	  this.quit();
-  }
-  
-  public void connectionClosed() {
-	  clientUI.display("***Connection to " + getHost() + ":" + getPort() + " closed.");
-  }
-  
-  /**
-   * This method terminates the client.
-   */
-  public void quit()
-  {
-    try
-    {
-      closeConnection();
-    }
-    catch(IOException e) {}
-    clientUI.display("Quiting SimpleChat client.");
-    System.exit(0);
-  }
+			break;
+		case "#setHost":
+			if (!isConnected()) {
+				setHost(arg);
+			} else {
+				clientUI.display("***ERROR*** Cannot change host while connected to server");
+			}
+			break;
+		case "#setPort":
+			if (!isConnected()) {
+				setPort(Integer.parseInt(arg));
+			} else {
+				clientUI.display("***ERROR*** Cannot change port while connected to server");
+			}
+			break;
+		case "#getHost":
+			clientUI.display("Host: " + getHost());
+			break;
+		case "#getPort":
+			clientUI.display("Port: " + getPort());
+			break;
+		case "#login":
+			login();
+			break;
+		}
+	}
+
+	public void connectionException(Exception exception) {
+		clientUI.display("Lost connection to server.\nQuitting...");
+		this.quit();
+	}
+
+	public void connectionClosed() {
+		clientUI.display("***Connection to " + getHost() + ":" + getPort() + " closed.");
+	}
+
+	/**
+	 * This method terminates the client.
+	 */
+	public void quit() {
+		try {
+			closeConnection();
+		} catch (IOException e) {
+		}
+		clientUI.display("> Quiting SimpleChat client.");
+		System.exit(0);
+	}
+
+	public String getLoginID() {
+		return loginID;
+	}
+
+	public void setLoginID(String loginID) {
+		this.loginID = loginID;
+	}
+	
+	public void login() {
+		try {
+			clientUI.display("Logging in at " + getHost() + ":" + getPort());
+			openConnection();
+			sendToServer("#login " + loginID);
+			clientUI.display("Logged in at " + getHost() + ":" + getPort());
+		} catch (IOException e) {
+			clientUI.display("Login failed at " + getHost() + ":" + getPort());
+		}
+	}
 }
-//End of ChatClient class
+// End of ChatClient class
